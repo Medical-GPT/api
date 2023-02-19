742eb1e977e008a8b1e5215b7d99c7d669455fef
Kristian Apostolov <kristian@pc-9-250.customer.ask4.lan>
2023-02-19 21:05:00 +0000

    Remove http endpoint

--- src/main.py
+++ src/main.py
@@ -17,12 +17,6 @@ app.add_middleware(
 )
 
 
-@app.post("/message")
-def message(message: Message):
-    response = get_message_response(message)
-    return {"message": response}
-
-
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
